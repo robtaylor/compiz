f7a08278f5cf4f32f7df42e69700e1edfa15fc37
Dennis Kasprzyk <onestone@compiz-fusion.org>
2008-09-24 20:01:33 +0200

    Apply libdecoration changes.

--- gtk/window-decorator/gtk-window-decorator.c
+++ gtk/window-decorator/gtk-window-decorator.c
@@ -7094,7 +7094,8 @@ main (int argc, char *argv[])
 	return 1;
     }
 
-    decor_set_dm_check_hint (xdisplay, gdk_screen_get_number (gdkscreen));
+    decor_set_dm_check_hint (xdisplay, gdk_screen_get_number (gdkscreen),
+			     WINDOW_DECORATION_TYPE_PIXMAP);
 
     update_default_decorations (gdkscreen);
 
