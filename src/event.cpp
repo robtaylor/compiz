167ca49ecdcae788f2353082a5d8c3ccaa41b4af
Danny Baumann <dannybaumann@web.de>
2008-10-05 13:08:28 +0200

    Ignore _NET_WM_STATE_HIDDEN property changes as suggested by EWMH.

--- src/event.cpp
+++ src/event.cpp
@@ -1161,6 +1161,13 @@ CompScreen::handleEvent (XEvent *event)
 		state = priv->getWindowState (w->id ());
 		state = CompWindow::constrainWindowState (state, w->actions ());
 
+		/* EWMH suggests that we ignore changes
+		   to _NET_WM_STATE_HIDDEN */
+		if (w->state () & CompWindowStateHiddenMask)
+		    state |= CompWindowStateHiddenMask;
+		else
+		    state &= ~CompWindowStateHiddenMask;
+
 		if (state != w->state ())
 		{
 		    w->state () = state;
