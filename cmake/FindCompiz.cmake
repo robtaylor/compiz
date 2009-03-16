dc3f4ce383309421ce5b9cd1eefd3684b3e4ca06
Dennis Kasprzyk <onestone@compiz-fusion.org>
2009-03-16 18:14:28 +0100

    Added version check.

--- cmake/FindCompiz.cmake
+++ cmake/FindCompiz.cmake
@@ -2,29 +2,45 @@
 
 if (NOT COMPIZ_INTERNAL)
 
-    include (FindPkgConfig)
-
-    if (COMPIZ_FIND_REQUIRED)
+    if (Compiz_FIND_REQUIRED)
 	set (_req REQUIRED)
     endif ()
 
-    set (PKGCONFIG_REGEX ".*\${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:\${CMAKE_INSTALL_PREFIX}/share/pkgconfig.*")
+    find_package (PkgConfig ${_req})
 
-    # add install prefix to pkgconfig search path if needed
-    if (NOT "$ENV{PKG_CONFIG_PATH}" MATCHES "${PKGCONFIG_REGEX}")
-	if ("" STREQUAL "$ENV{PKG_CONFIG_PATH}")
-	    set (ENV{PKG_CONFIG_PATH} "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:${CMAKE_INSTALL_PREFIX}/share/pkgconfig")
-	else ()
-	    set (ENV{PKG_CONFIG_PATH}
-		"${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:${CMAKE_INSTALL_PREFIX}/share/pkgconfig:$ENV{PKG_CONFIG_PATH}")
+    if (PKG_CONFIG_FOUND)
+
+	set (_comp_ver)
+	if (Compiz_FIND_VERSION)
+	    if (Compiz_FIND_VERSION_EXACT)
+		set (_comp_ver "=${Compiz_FIND_VERSION}")
+	    else ()
+		set (_comp_ver ">=${Compiz_FIND_VERSION}")
+	    endif ()
 	endif ()
-    endif ()
 
-    pkg_check_modules (COMPIZ ${_req} compiz)
+	set (PKGCONFIG_REGEX ".*\${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:\${CMAKE_INSTALL_PREFIX}/share/pkgconfig.*")
 
-    if (COMPIZ_FOUND)
-	set (CMAKE_MODULE_PATH  ${CMAKE_MODULE_PATH} ${COMPIZ_PREFIX}/share/compiz/cmake)
-	include (CompizDefaults)
+	# add install prefix to pkgconfig search path if needed
+	if (NOT "$ENV{PKG_CONFIG_PATH}" MATCHES "${PKGCONFIG_REGEX}")
+	    if ("" STREQUAL "$ENV{PKG_CONFIG_PATH}")
+		set (ENV{PKG_CONFIG_PATH} "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:${CMAKE_INSTALL_PREFIX}/share/pkgconfig")
+	    else ()
+		set (ENV{PKG_CONFIG_PATH}
+		    "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig:${CMAKE_INSTALL_PREFIX}/share/pkgconfig:$ENV{PKG_CONFIG_PATH}")
+	    endif ()
+	endif ()
+
+	pkg_check_modules (COMPIZ ${_req} "compiz${_comp_ver}")
+
+	find_path(_compiz_def_macro CompizDefaults.cmake ${COMPIZ_PREFIX}/share/compiz/cmake)
+
+	if (COMPIZ_FOUND AND _compiz_def_macro)
+	    set (CMAKE_MODULE_PATH  ${CMAKE_MODULE_PATH} ${COMPIZ_PREFIX}/share/compiz/cmake)
+	    include (CompizDefaults)
+	elseif (Compiz_FIND_REQUIRED)
+	    message (FATAL_ERROR "Unable to find Compiz ${_comp_ver}")
+	endif ()
     endif ()
 endif ()
 
