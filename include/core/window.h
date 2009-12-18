5ce57a6682f936308e8c8769bac0c4c636c0b8ab
Erkin Bahceci <erkinbah@gmail.com>
2009-12-18 00:59:35 -0600

    Add CompWindowGrabExternalAppMask.

--- include/core/window.h
+++ include/core/window.h
@@ -149,11 +149,11 @@ struct CompStartupSequence;
 #define ClientTypeApplication  1
 #define ClientTypePager        2
 
-#define CompWindowGrabKeyMask    (1 << 0)
-#define CompWindowGrabButtonMask (1 << 1)
-#define CompWindowGrabMoveMask   (1 << 2)
-#define CompWindowGrabResizeMask (1 << 3)
-
+#define CompWindowGrabKeyMask         (1 << 0)
+#define CompWindowGrabButtonMask      (1 << 1)
+#define CompWindowGrabMoveMask        (1 << 2)
+#define CompWindowGrabResizeMask      (1 << 3)
+#define CompWindowGrabExternalAppMask (1 << 4)
 
 enum CompStackingUpdateMode {
     CompStackingUpdateModeNone = 0,
