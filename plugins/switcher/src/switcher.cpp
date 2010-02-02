5d4543c90bbc816cdc3c1e54e1e23d4f44f98e7a
Sam Spilsbury <smspillaz@gmail.com>
2010-02-02 17:09:35 +0800

    Revert "Correctly reflect that the switcher window is managed."
    This reverts commit b27402c2460592bc3ed6c41c2b7d5cae47e79079.
    
    We should not allow plugins to directly write to w->managed, some core event should be used to set managed and recalculate the window state.

--- plugins/switcher/src/switcher.cpp
+++ plugins/switcher/src/switcher.cpp
@@ -305,13 +305,7 @@ SwitchScreen::initiate (SwitchWindowSelection selection,
 		if (w && (w->state () & CompWindowStateHiddenMask))
 		    w->show ();
 		else
-		{
 		    XMapWindow (screen->dpy (), popupWindow);
-		    /* we don't get a MapRequest for internal window
-		       creations, so we need to set the managed state
-		       ourselves */
-		    w->managed = TRUE;
-		}
 
 		setSelectedWindowHint ();
 	    }
