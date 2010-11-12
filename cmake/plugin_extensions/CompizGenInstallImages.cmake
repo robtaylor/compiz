868eeabb474b29776077d4cfdc8608469ae1ce09
Sam Spilsbury <sam.spilsbury@canonical.com>
2010-11-12 19:45:55 +0800

    Install plugin images and data to separate plugin-specific subdirectories (so that plugins can't overwrite other plugin's data). Also fix typo in CompizGenInstallData.cmake

--- cmake/plugin_extensions/CompizGenInstallImages.cmake
+++ cmake/plugin_extensions/CompizGenInstallImages.cmake
@@ -1,15 +1,15 @@
 # determinate installation directories
 macro (compiz_images_prepare_dirs)
     if ("${COMPIZ_PLUGIN_INSTALL_TYPE}" STREQUAL "package")
-	set (PLUGIN_IMAGEDIR   ${datadir}/compiz)
+	set (PLUGIN_IMAGEDIR   ${datadir}/compiz/${COMPIZ_CURRENT_PLUGIN})
 
     elseif ("${COMPIZ_PLUGIN_INSTALL_TYPE}" STREQUAL "compiz" OR
 	    "$ENV{BUILD_GLOBAL}" STREQUAL "true")
-	set (PLUGIN_IMAGEDIR   ${COMPIZ_PREFIX}/share/compiz)
+	set (PLUGIN_IMAGEDIR   ${COMPIZ_PREFIX}/share/compiz/${COMPIZ_CURRENT_PLUGIN})
 
     else ("${COMPIZ_PLUGIN_INSTALL_TYPE}" STREQUAL "compiz" OR
 	  "$ENV{BUILD_GLOBAL}" STREQUAL "true")
-	set (PLUGIN_IMAGEDIR   $ENV{HOME}/.compiz-1)
+	set (PLUGIN_IMAGEDIR   $ENV{HOME}/.compiz-1/${COMPIZ_CURRENT_PLUGIN})
 
     endif ("${COMPIZ_PLUGIN_INSTALL_TYPE}" STREQUAL "package")
 endmacro (compiz_images_prepare_dirs)
