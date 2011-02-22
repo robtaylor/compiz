b9cbf7a14aa8f1f0d4415064eb7488187a817f54
Sam Spilsbury <sam.spilsbury@canonical.com>
2011-02-22 17:59:57 +0800

    s/Novell Inc./Canonical Ltd./ in eventsource.cpp copyright

--- src/eventsource.cpp
+++ src/eventsource.cpp
@@ -6,9 +6,9 @@
  * fee, provided that the above copyright notice appear in all copies
  * and that both that copyright notice and this permission notice
  * appear in supporting documentation, and that the name of
- * Novell, Inc. not be used in advertising or publicity pertaining to
+ * Canonical Ltd. not be used in advertising or publicity pertaining to
  * distribution of the software without specific, written prior permission.
- * Novell, Inc. makes no representations about the suitability of this
+ * Canonical Ltd. makes no representations about the suitability of this
  * software for any purpose. It is provided "as is" without express or
  * implied warranty.
  *
